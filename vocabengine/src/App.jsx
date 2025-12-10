f7d05aa031bb4217aefcecb3b262c799c27a4b9e
Arsh1333 <pawar.arsh1333@gmail.com>
2025-12-10 22:20:59 +0530

    first commit

---
+++ vocabengine/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <h1 class="text-3xl font-bold underline bg-amber-200">Hello world!</h1>
+        <button class="btn btn-primary">Button</button>
+      </div>
+    </>
+  );
+}
+
+export default App
