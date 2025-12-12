2dabde124a79cb934a4660954e1e8b45c05d7295
Arsh1333 <pawar.arsh1333@gmail.com>
2025-12-12 15:49:55 +0530

    loading contents from text file

--- vocabengine/src/App.jsx
+++ vocabengine/src/App.jsx
@@ -1,25 +1,43 @@
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [commonWords, setCommonWords] = useState(null);
 
+  const loadCommonWords = async () => {
+    try {
+      const res = await fetch("/wordsData/commonWords.txt");
+      const text = await res.text();
+
+      const commonWordSet = new Set(
+        text
+          .split("\n")
+          .map((w) => w.trim().toLowerCase())
+          .filter(Boolean)
+      );
+
+      return commonWordSet;
+    } catch (error) {
+      console.log("error while loading words file", error);
+    }
+  };
+
+  useEffect(() => {
+    const fetchWords = async () => {
+      const set = await loadCommonWords();
+      setCommonWords(set);
+      console.log("Loaded common words:", set);
+    };
+
+    fetchWords();
+  }, []);
 
   return (
     <>
       <div>
-        {/* <h1 class="text-3xl font-bold underline bg-amber-200">Hello world!</h1>
-        <button class="btn btn-primary">Button</button> */}
         <div className="join">
           <div>
             <label className="input validator join-item">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                className="size-4"
-              >
-                <path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32L19.513 8.2Z" />
-              </svg>
-
               <input type="text" placeholder="Enter your text" />
             </label>
           </div>
@@ -30,4 +48,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
