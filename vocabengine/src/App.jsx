dd8f3352b17cef7a0286509704f9e8a8958ec2b0
Arsh1333 <pawar.arsh1333@gmail.com>
2025-12-18 22:39:06 +0530

    tokenization and displaying uncommon words

--- vocabengine/src/App.jsx
+++ vocabengine/src/App.jsx
@@ -3,6 +3,8 @@ import "./App.css";
 
 function App() {
   const [commonWords, setCommonWords] = useState(null);
+  const [inputText, setInputText] = useState("");
+  const [uncommonWords, setUncommonWords] = useState([]);
 
   const loadCommonWords = async () => {
     try {
@@ -22,6 +24,40 @@ function App() {
     }
   };
 
+  const extractWord = (text) => {
+    let words = [];
+    let current = "";
+    let textToLowerCase = text.toLowerCase();
+    for (let i of textToLowerCase) {
+      if (i >= "a" && i <= "z") {
+        current += i;
+      } else {
+        if (current.length > 0) {
+          words.push(current);
+          current = "";
+        }
+      }
+    }
+    return words;
+  };
+
+  const findUncommonWords = (text, commonWordSet) => {
+    if (!commonWordSet) {
+      console.log("common words set not found");
+      return;
+    }
+    const words = extractWord(text);
+    const result = new Set();
+
+    for (let i of words) {
+      if (!commonWordSet.has(i) && i.length > 2) {
+        result.add(i);
+      }
+    }
+
+    return Array.from(result);
+  };
+
   useEffect(() => {
     const fetchWords = async () => {
       const set = await loadCommonWords();
@@ -38,10 +74,30 @@ function App() {
         <div className="join">
           <div>
             <label className="input validator join-item">
-              <input type="text" placeholder="Enter your text" />
+              <input
+                type="text"
+                placeholder="Enter your text"
+                value={inputText}
+                onChange={(e) => setInputText(e.target.value)}
+              />
             </label>
           </div>
-          <button className="btn btn-neutral join-item ml-4">Extract</button>
+          <button
+            className="btn btn-neutral join-item ml-4"
+            onClick={() => {
+              const words = findUncommonWords(inputText, commonWords);
+              setUncommonWords(words);
+            }}
+          >
+            Extract
+          </button>
+        </div>
+        <div className="mt-6">
+          {uncommonWords.map((word) => (
+            <span key={word} className="badge badge-outline mr-2 mb-2">
+              {word}
+            </span>
+          ))}
         </div>
       </div>
     </>
