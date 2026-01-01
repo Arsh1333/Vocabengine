dc5f4e8682713a1a08b40d532f6d6ca07b34cb8f
Arsh1333 <pawar.arsh1333@gmail.com>
2026-01-01 22:31:57 +0530

    dictionary api for meaning

--- vocabengine/src/App.jsx
+++ vocabengine/src/App.jsx
@@ -5,7 +5,21 @@ function App() {
   const [commonWords, setCommonWords] = useState(null);
   const [inputText, setInputText] = useState("");
   const [uncommonWords, setUncommonWords] = useState([]);
-  const [frequencyMap, setFrequencyMap] = useState(null);
+
+
+  const meaning = async () => {
+    try {
+      const res = await fetch(
+        `https://api.dictionaryapi.dev/api/v2/entries/en/hello`
+      );
+      console.log(res);
+    } catch (error) {
+      console.log(
+        "Error while getting meaning from meaning from dictionary api",
+        error
+      );
+    }
+  };
 
   const loadCommonWords = async () => {
     try {
@@ -22,28 +36,6 @@ function App() {
       return commonWordSet;
     } catch (error) {
       console.log("error while loading words file", error);
-    }
-  };
-
-  const loadFrequencyWords = async () => {
-    try {
-      const res = await fetch("/wordsData/unigram_freq.csv");
-      const text = await res.text();
-      const lines = text.split("\n");
-      const map = new Map();
-      for (let i = 0; i < lines.length; i++) {
-        const line = lines[i].trim();
-        if (!line) {
-          continue;
-        }
-        const [word, count] = line.split(",");
-        map.set(word.toLowerCase(), Number(count));
-      }
-      setFrequencyMap(map);
-      console.log(map);
-      console.log("Loaded frequency words:", map.size);
-    } catch (error) {
-      console.log("error while loading frequency file", error);
     }
   };
 
@@ -92,7 +84,7 @@ function App() {
   }, []);
 
   useEffect(() => {
-    loadFrequencyWords();
+    meaning();
   }, [uncommonWords]);
 
   return (
